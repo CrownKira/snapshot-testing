068ea4f40aeb7e41a7314a28c4e30889231c0611
CrownKira <t.karwi@yahoo.com>
2021-04-08 20:05:56 +0800

    Make minor changes to comments

--- __tests__/link.react.test.js
+++ __tests__/link.react.test.js
@@ -6,16 +6,13 @@ import React from 'react';
 import Link from '../Link.react';
 import renderer from 'react-test-renderer';
 
-/// rules:
-/// should create snapshot files
+// use either it() or test()
+// expect this rect component returned value to match snapshots
+// will create snapshots on first run
+// afterwards, all the test will be compared to the originally generated snapshots
+// --testNamePattern flag to re-record snapshots only for those tests that match the pattern.
 
-/// use it() or test()
-/// expect this rect component returned value to match snapshots
-/// will create snapshots on first run
-/// afterwards, all the test will be compared to the originally generated snapshots
-/// --testNamePattern flag to re-record snapshots only for those tests that match the pattern.
-
-/// yarn test -- -u : update snapshots
+// yarn test -- -u : update snapshots
 it('renders correctly', () => {
   const tree = renderer
     .create(<Link page="http://www.instagram.com">Instagram</Link>)
@@ -23,8 +20,9 @@ it('renders correctly', () => {
   expect(tree).toMatchSnapshot();
 });
 
-/// First, you write a test, calling .toMatchInlineSnapshot() with no arguments:
-/// The next time you run Jest, tree will be evaluated, and a snapshot will be written as an argument to toMatchInlineSnapshot:
+// First, you write a test, calling .toMatchInlineSnapshot() with no arguments:
+// The next time you run Jest, tree will be evaluated,
+// and a snapshot will be written as an argument to toMatchInlineSnapshot:
 it('renders as an anchor when no page is set', () => {
   const tree = renderer.create(<Link>Facebook</Link>).toJSON();
   expect(tree).toMatchInlineSnapshot(`
--- __tests__/property-matchers.test.js
+++ __tests__/property-matchers.test.js
@@ -1,16 +1,18 @@
 Date.now = jest.fn(() => 1482363367071);
 
-it("will fail every time", () => {
+it('will fail every time', () => {
   const user = {
-    /// test should be deterministic
+    // test should be deterministic
     createdAt: Date.now,
     id: Math.floor(Math.random() * 20),
-    name: "LeBron James",
+    name: 'LeBron James',
   };
 
-  /// For these cases, Jest allows providing an asymmetric matcher for any property. These matchers are checked before the snapshot is written or tested, and then saved to the snapshot file instead of the received value:
-  /// use expect.any()
-  /// so it takes in any date or any number
+  // For these cases, Jest allows providing an asymmetric matcher for any property.
+  // These matchers are checked before the snapshot is written or tested,
+  // and then saved to the snapshot file instead of the received value:
+  // use expect.any()
+  // so it takes in any date or any number
   expect(user).toMatchInlineSnapshot(
     {
       id: expect.any(Number),
